ee61c35d2e02fdfddbfc7ae5706e4674fa944d55
ivanreynaldo <78341773+ivanreynaldo@users.noreply.github.com>
2023-01-04 10:01:10 +0700

    Add files via upload

---
+++ ProjectPOS/src/main/java/com/rplbo/uasprojectpos/PointOfSales.java
@@ -0,0 +1,18 @@
+package com.rplbo.uasprojectpos;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class PointOfSales {
+    private ArrayList<Barang> arrBarang;
+
+    public PointOfSales() {    }
+
+    public void inputBarang(Barang barang){
+
+    }
+
+    public void tampilDaftarBarang(){}
+
+    public void checkout(HashMap<Barang,Integer> hashMap,long angka){}
+}
